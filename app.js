6007bc8cfae82832bb47c752e86f5218a9098493
rasuljonadhamov <rasuljonadhamov@gmail.com>
2023-11-03 10:18:06 +0300

    Add localStorage to the project

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 const nameInput = document.getElementById("username");
+const nameUser = document.getElementById("nameUser");
+const language = document.getElementById("language");
+const ageUser = document.getElementById("age");
 const passwordInput = document.getElementById("password");
 const btn = document.getElementById("btn");
 const form = document.getElementById("form");
 const message = document.querySelector(".error-message1");
+
+// localStorage.clear();
+
+const userArray = [];
 
 btn.addEventListener("click", function (e) {
   e.preventDefault();
 
   if (passwordInput.value.length <= 6) {
     message.innerText = "Password must be at least 8 character";
-  } else {
-    passwordInput.value = "";
-    nameInput.value = "";
-  }
-
-  if (passwordInput.value.length >= 14) {
+  } else if (passwordInput.value.length >= 14) {
     message.innerText = "Password must be between 8 and 13 character";
   } else {
+    userArray.push(passwordInput.value);
+    userArray.push(nameInput.value);
+    // userArray.push(nameUser.value);
+    userArray.push(ageUser.value);
+    userArray.push(language.value);
+    localStorage.setItem("user", JSON.stringify(userArray));
     passwordInput.value = "";
     nameInput.value = "";
+    ageUser.value = "";
+    language.value = "";
+    console.log(userArray);
   }
 
   if (nameInput.value) {
